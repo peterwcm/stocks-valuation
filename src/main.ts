184f4381fcaa0035ff894418e65e3819fc82b5b2
Peter Wong <peterwong.brisbane@gmail.com>
2020-03-21 09:46:49 +0800

    basic Buefy setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'
+})
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="stocks">
     <h1>Stocks</h1>
+    <b-icon icon="home"></b-icon>
   </div>
 </template>
