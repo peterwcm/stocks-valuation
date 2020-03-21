e5c723511d67198c7038552d622b065e0e3dc37b
Peter Wong <peterwong.brisbane@gmail.com>
2020-03-21 18:33:38 +0800

    import stock css

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -83,7 +83,6 @@ export default class Stock extends Vue {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "@/styles/components/Stock";
+@import "@/styles/components/_stock";
 </style>
