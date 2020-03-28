56a23c0b6705355ed97c99ddfb678d7e4a3c8b4c
Peter Wong <peterwong.brisbane@gmail.com>
2020-03-28 09:53:16 +0800

    less strict eslinting

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    '@typescript-eslint/no-inferrable-types': 'off'
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 };
