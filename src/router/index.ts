bf93a6231db8a4646f51dae1412aaea2fe3579c7
Peter Wong <peterwong.brisbane@gmail.com>
2020-06-01 18:06:14 +0800

    reformat index JS

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,26 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '@/views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '@/views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [{
+const routes = [
+  {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/stocks',
     name: 'Stocks',
-    component: () => import(/* webpackChunkName: "stocks" */ '@/views/Stocks.vue')
-}]
+    component: () => import(/* webpackChunkName: "stocks" */ '@/views/Stocks.vue'),
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
