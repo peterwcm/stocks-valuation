24ff738f96fe882071669dd140acb4d0d9e23b1e
Peter Wong <peterwong.brisbane@gmail.com>
2020-06-12 22:17:31 +0800

    dummy Login Form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <b-field>
+      <b-input placeholder="Username" icon="user" maxlength="30"></b-input>
+    </b-field>
+
+    <b-field>
+      <b-input placeholder="Password" type="password" icon="key" password-reveal></b-input>
+    </b-field>
+
+    <b-field>
+      <b-button tag="input" type="is-primary" native-type="submit" value="Login" />
+    </b-field>
+  </form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    async loginUser() {
+      return false;
+    }
+  }
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,13 +9,22 @@
     </section>
     <section class="section">
       <div class="container">
-        <b-field label="Username">
-          <b-input placeholder="Username" icon="user" maxlength="30"></b-input>
-        </b-field>
-        <b-field label="Password">
-          <b-input placeholder="Password" type="password" icon="key" password-reveal></b-input>
-        </b-field>
+        <div class="columns is-centered">
+          <div class="column is-one-third-desktop">
+            <LoginForm />
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import LoginForm from "@/components/LoginForm.vue";
+
+export default {
+  components: {
+    LoginForm
+  }
+};
+</script>
