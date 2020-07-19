91b065e4539bc08ab85604fc84cada5db3806451
Peter Wong <peterwong.brisbane@gmail.com>
2020-07-20 00:38:27 +0800

    removed socketIO code

--- srv/index.js
+++ srv/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-// import socketIO from "socket.io";
 
-export default (app, http) => {
+export default (app) => {
   const users = require('./routes/api/users');
   const stocks = require('./routes/api/stocks');
 
@@ -10,14 +9,4 @@ export default (app, http) => {
   app.use(cors());
   app.use('/api/users', users);
   app.use('/api/stocks', stocks);
-
-  // optional support for socket.io
-  //
-  // let io = socketIO(http);
-  // io.on("connection", client => {
-  //   client.on("message", function(data) {
-  //     // do something
-  //   });
-  //   client.emit("message", "Welcome");
-  // });
 };
