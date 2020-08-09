32e4a42c0b217b5c2f08ee1cd5eb2da1b7dff16d
Peter Wong <peterwong.brisbane@gmail.com>
2020-08-09 10:17:19 +0800

    devserver config fix

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,17 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    proxy: 'http://localhost:3000',
+  },
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
-      patterns: [path.resolve(__dirname, './src/styles/abstracts/_index.scss')]
+      patterns: [path.resolve(__dirname, './src/styles/abstracts/_index.scss')],
     },
     express: {
       shouldServeApp: true,
-      serverDir: './srv'
-    }
-  }
+      serverDir: './srv',
+    },
+  },
 };
